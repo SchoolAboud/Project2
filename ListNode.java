0eab45a48263a5f77a41338a450f7a250b3f6d11
SchoolAboud <59705058+SchoolAboud@users.noreply.github.com>
2020-05-19 14:31:52 -0700

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,25 @@
+/* CS211, 5/17/2020, Abdalrahman Shaath
+ListNode is a class for storing a single node of a linked
+list.  This node class is for a list of integer values.
+ Any alteration will be at the bottom of the code.*/
+
+public class ListNode {
+    public int data;       // data stored in this node
+    public ListNode next;  // link to next node in the list
+
+    // post: constructs a node with given data and given link
+    public ListNode(int data, ListNode next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    // post: constructs a node with given data and null link
+    public ListNode(int data) {
+        this(data, null);
+    }
+
+    // post: constructs a node with data 0 and null link
+    public ListNode() {
+        this(0, null);
+    }
+}
