886bca1f525085d3a361a661a7aade5626c37786
SchoolAboud <59705058+SchoolAboud@users.noreply.github.com>
2020-05-24 07:13:34 -0700

    Add files via upload

---
+++ Team7Project#2/ArrayList.java
@@ -0,0 +1,131 @@
+/*CS211, 5/18/2020, Team 7[Majd Chalak, Abdalrahman(Aboud) Shaath, William(Liam) Bryan, Joseph Helf, Richard Le],
+This is the Array List class it extends AbstractList*/
+
+// Class ArrayList<E> can be used to store a list of values of type E.
+
+import java.util.*;
+import java.util.Iterator;
+
+
+public class ArrayList<E> extends AbstractList<E> {
+    private E[] elementData; // list of values
+    //private int size;        // No need for it since it's already present in abstract.
+  
+    public static final int DEFAULT_CAPACITY = 100;
+
+    // pre : capacity >= 0 (throws IllegalArgumentException if not)
+    // post: constructs an empty list with the given capacity
+    @SuppressWarnings("unchecked")
+    public ArrayList(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("capacity: " + capacity);
+        }
+        elementData = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    // post: constructs an empty list of default capacity
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    // pre : 0 <= index <= size() (throws IndexOutOfBoundsException if not)
+    // post: inserts the given value at the given index, shifting subsequent
+    //       values right
+    public void add(int index, E value) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("index: " + index);
+        }
+        ensureCapacity(size + 1);
+      
+        for (int i = size; i >= index + 1; i--) {
+            elementData[i] = elementData[i - 1];
+        }
+        elementData[index] = value;
+        size++;
+    }
+
+    // pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
+    // post: removes value at the given index, shifting subsequent values left
+    public void remove(int index) {
+
+          checkIndex(index);
+          for (int i = index; i < size - 1; i++) {
+              elementData[i] = elementData[i + 1];
+          }
+          elementData[size - 1] = null;
+          size--;
+     }
+    
+
+    // post: list is empty
+       public void clear() {
+             for (int i = 0; i < size; i++) {
+                 elementData[i] = null;
+             }
+           this.size = 0;
+       }
+
+    // post: returns an iterator for this list
+    public Iterator<E> iterator() {
+        return new ArrayListIterator();
+    }
+
+    // post: ensures that the underlying array has the given capacity; if not,
+    //       the size is doubled (or more if given capacity is even larger)
+    public void ensureCapacity(int capacity) {
+        if (capacity > elementData.length) {
+            int newCapacity = elementData.length * 2 + 1;
+            if (capacity > newCapacity) {
+                newCapacity = capacity;
+            }
+            elementData = Arrays.copyOf(elementData, newCapacity);
+        }
+    }
+
+    private class ArrayListIterator implements Iterator<E> {
+        private int position;           // current position within the list
+        private boolean removeOK;       // whether it's okay to remove now
+
+        // post: constructs an iterator for the given list
+        public ArrayListIterator() {
+            position = 0;
+            removeOK = false;
+        }
+
+        // post: returns true if there are more elements left, false otherwise
+        public boolean hasNext() {
+            return position < size;
+        }
+
+        // pre : hasNext() (throws NoSuchElementException if not)
+        // post: returns the next element in the iteration
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            E result = elementData[position];
+            position++;
+            removeOK = true;
+            return result;
+        }
+
+        // pre : next() has been called without a call on remove (throws
+        //       IllegalStateException if not)
+        // post: removes the last element returned by the iterator
+        public void remove() {
+            if (!removeOK) {
+                throw new IllegalStateException();
+            }
+            ArrayList.this.remove(position - 1);
+            position--;
+            removeOK = false;
+        }
+      
+        public void add(E value) {
+                
+        }
+        
+    }
+}
+
---
+++ Team7Project#2/List.java
@@ -0,0 +1,17 @@
+/*CS211, 5/18/2020, Team 7[Majd Chalak, Abdalrahman(Aboud) Shaath, William(Liam) Bryan, Joseph Helf, Richard Le],
+This is the interface List*/
+
+public interface List<E> extends Iterable<E> {
+    public int size();
+    public E get(int index);
+    public int indexOf(E value);
+    public boolean isEmpty();
+    public boolean contains(E value);
+    public void add(E value);
+    public void add(int index, E value);
+    public void addAll(List<E> other);
+    public void remove(int index);
+    public void set(int index, E value);
+    public void clear();
+    
+}
